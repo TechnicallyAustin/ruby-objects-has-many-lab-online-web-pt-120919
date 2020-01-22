32699de4ff687bcdd8426ee08064a2c6ef7f7a55
Austin Johnson <technicallyaustin@gmail.com>
2020-01-22 20:10:51 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,27 @@
 class Author 
+  attr_accessor :name, :post 
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts
+    Post.all
+  end
+  
+  def add_post(pos)
+    @posts << pos 
+    Post.all << pos
+  end
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+  end
+  def self.post_count
+    Post.all.count 
+  end
+  
   
   
 end
