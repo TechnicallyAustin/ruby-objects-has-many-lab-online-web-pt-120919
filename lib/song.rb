fbd4fda2f1afa1362fdf5badb75521fe6dba8b0a
Austin Johnson <technicallyaustin@gmail.com>
2020-01-22 19:51:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,9 +12,10 @@ class Song
   end
   
   def artist_name
-    if self.artist.name == nil?
+    if self.artist.name == false 
      nil
     else
+      self.artist.name == true 
       self.artist.name 
     end
     binding.pry
